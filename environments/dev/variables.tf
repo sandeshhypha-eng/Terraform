364f375f1d55dbebd77b4ff8fdea5cb50e7a5aa8
sandeshhypha-eng <sandeshhyhpha@gmail.com>
2025-11-14 23:31:00 +0530

    xxx

--- environments/dev/variables.tf
+++ environments/dev/variables.tf
@@ -14,7 +14,7 @@
 variable "aws_region" {
   description = "AWS region for resources"
   type        = string
-  default     = "us-east-1"
+  default     = "ap-south-1"
 }
 
 # ============================================================================
--- environments/release/main.tf
+++ environments/release/main.tf
@@ -19,7 +19,7 @@ terraform {
   backend "s3" {
     bucket         = "git-hpha-terraform-state"
     key            = "terraformv2/environments/release/terraform.tfstate"
-    region         = "us-east-1"
+    region         = "ap-south-1"
     encrypt        = true
     use_lockfile = true
     # S3 native locking explanation:
