d9cd7aebccc58eca1f4b8a0b6cd7877042cdbc54
sandeshhypha-eng <sandeshhypha@gmail.com>
2026-05-19 21:06:01 +0530

    added

--- demo.tf
+++ demo.tf
@@ -8,7 +8,7 @@ terraform {
   }
   backend "s3" {
     bucket = "devhyphastatefile"
-    key    = "yogesh/dev.tfstate"
+    key    = var.statefilekey
     region = "ap-south-1"
   }
 }
@@ -24,7 +24,9 @@ resource "random_string" "random_string_for_s3_bucket" {
 variable "country" {
   type = string
 }
-
+variable "statefilekey" {
+  type = string
+}
 variable "os" {
   type = string
 }
