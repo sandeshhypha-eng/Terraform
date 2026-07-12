750c6a37c8e00bf3d0baeb3ec81a7b8534bcd1d3
sandeshhypha-eng <sandeshhypha@gmail.com>
2026-07-12 18:39:38 +0530

    asf

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -19,7 +19,7 @@ terraform {
   backend "s3" {
     bucket         = "git-hpha-terraform-state-v2"
     key            = "terraformv2/environments/dev/terraform.tfstate"
-    region         = "us-east-1"
+    region         = "ap-south-1"
     encrypt        = true
     use_lockfile = true
     # S3 native locking explanation:
